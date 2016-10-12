8925578cc0ad464dba78c0a101b6ba54d30f9b47
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-12 20:09:16 +0700

    Redirect to /login if not signed in yet

--- client/app/questionsCreate/questionsCreate.controller.js
+++ client/app/questionsCreate/questionsCreate.controller.js
@@ -2,6 +2,15 @@
 
 angular.module('superQa3App')
 	.controller('QuestionsCreateCtrl', function ($scope, $http, $location) {
+
+		// Redirect to /login if not signed in yet
+		if (!Auth.isLoggedIn()) {
+			$location.path('/login');
+			$location.replace();
+			return;
+		}
+
+		// Get all questions from server
 		$scope.submit = function () {
 			$http.post('/api/questions', $scope.question).success(function () {
 				$location.path('/')
