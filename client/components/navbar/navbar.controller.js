8041c726f2a56d2655afcb130204e7ca460166f9
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-12 19:01:09 +0700

    styling

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,21 +1,21 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
+	//start-non-standard
+	menu = [{
+		'title': 'Questions',
+		'state': 'main'
   }];
 
-  isCollapsed = true;
-  //end-non-standard
+	isCollapsed = true;
+	//end-non-standard
 
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
+	constructor(Auth) {
+		this.isLoggedIn = Auth.isLoggedIn;
+		this.isAdmin = Auth.isAdmin;
+		this.getCurrentUser = Auth.getCurrentUser;
+	}
 }
 
 angular.module('superQa3App')
-  .controller('NavbarController', NavbarController);
+	.controller('NavbarController', NavbarController);
