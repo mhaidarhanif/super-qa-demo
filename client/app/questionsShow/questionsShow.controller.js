981f224d2e41e58979813cfdd4317f1d6c5b6673
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-12 19:07:09 +0700

    answer controller+view in question

--- client/app/questionsShow/questionsShow.controller.js
+++ client/app/questionsShow/questionsShow.controller.js
@@ -2,10 +2,18 @@
 
 angular.module('superQa3App')
 	.controller('QuestionsShowCtrl', function ($scope, $http, $stateParams) {
-		var loadQuestions = function () {
+		let loadQuestions = function () {
 			$http.get('/api/questions/' + $stateParams.id).success(function (question) {
 				$scope.question = question
 			})
 		}
 		loadQuestions()
+
+		$scope.newAnswer = {}
+		$scope.submitAnswer = function () {
+			$http.post('/api/questions/' + $stateParams.id + '/answers', $scope.newAnswer).success(function () {
+				loadQuestions()
+				$scope.newAnswer = {}
+			})
+		}
 	})
--- server/api/question/question.model.js
+++ server/api/question/question.model.js
@@ -5,6 +5,9 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var QuestionSchema = new mongoose.Schema({
 	title: String,
 	content: String,
+	answers: [{
+		content: String
+  }],
 	active: Boolean
 });
 
