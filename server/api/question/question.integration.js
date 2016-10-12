1e3053fd02e51dda266b1903cfbd01e042009fb6
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-12 20:13:14 +0700

    question: integration test with auth

--- server/api/question/question.integration.js
+++ server/api/question/question.integration.js
@@ -1,14 +1,48 @@
-'use strict';
+'use strict'
 
-var app = require('../..');
-import request from 'supertest';
+import request from 'supertest'
 
-var newQuestion;
+const app = require('../..')
+const User = require('../user/user.model')
+
+let newQuestion
 
 describe('Question API:', function () {
 
+	// Need authentication
+
+	var user
+	before(function () {
+		return User.removeAsync().then(function () {
+			user = new User({
+				name: 'Fake User',
+				email: 'test@example.com',
+				password: 'password'
+			})
+
+			return user.saveAsync()
+		})
+	})
+
+	var token
+	before(function (done) {
+		request(app)
+			.post('/auth/local')
+			.send({
+				email: 'test@example.com',
+				password: 'password'
+			})
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function (err, res) {
+				token = res.body.token
+				done()
+			})
+	})
+
+	// It's fine without authentication
 	describe('GET /api/questions', function () {
-		var questions;
+		var questions
 
 		beforeEach(function (done) {
 			request(app)
@@ -17,23 +51,25 @@ describe('Question API:', function () {
 				.expect('Content-Type', /json/)
 				.end((err, res) => {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					questions = res.body;
-					done();
-				});
-		});
+					questions = res.body
+					done()
+				})
+		})
 
 		it('should respond with JSON array', function () {
-			expect(questions).to.be.instanceOf(Array);
-		});
+			expect(questions).to.be.instanceOf(Array)
+		})
 
-	});
+	})
 
+	// Need authentication
 	describe('POST /api/questions', function () {
 		beforeEach(function (done) {
 			request(app)
 				.post('/api/questions')
+				.set('authorization', 'Bearer ' + token) // need auth
 				.send({
 					title: 'New Question',
 					content: 'This is the brand new question!!!'
@@ -42,54 +78,56 @@ describe('Question API:', function () {
 				.expect('Content-Type', /json/)
 				.end((err, res) => {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					newQuestion = res.body;
-					done();
-				});
-		});
+					newQuestion = res.body
+					done()
+				})
+		})
 
 		it('should respond with the newly created question', function () {
-			expect(newQuestion.title).to.equal('New Question');
-			expect(newQuestion.content).to.equal('This is the brand new question!!!');
-		});
+			expect(newQuestion.title).to.equal('New Question')
+			expect(newQuestion.content).to.equal('This is the brand new question!!!')
+		})
 
-	});
+	})
 
 	describe('GET /api/questions/:id', function () {
-		var question;
+		var question
 
 		beforeEach(function (done) {
 			request(app)
 				.get('/api/questions/' + newQuestion._id)
+				.set('authorization', 'Bearer ' + token) // need auth
 				.expect(200)
 				.expect('Content-Type', /json/)
 				.end((err, res) => {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					question = res.body;
-					done();
-				});
-		});
+					question = res.body
+					done()
+				})
+		})
 
 		afterEach(function () {
-			question = {};
-		});
+			question = {}
+		})
 
 		it('should respond with the requested question', function () {
-			expect(question.title).to.equal('New Question');
-			expect(question.content).to.equal('This is the brand new question!!!');
-		});
+			expect(question.title).to.equal('New Question')
+			expect(question.content).to.equal('This is the brand new question!!!')
+		})
 
-	});
+	})
 
 	describe('PUT /api/questions/:id', function () {
-		var updatedQuestion;
+		var updatedQuestion
 
 		beforeEach(function (done) {
 			request(app)
 				.put('/api/questions/' + newQuestion._id)
+				.set('authorization', 'Bearer ' + token) // need auth
 				.send({
 					title: 'Updated Question',
 					content: 'This is the updated question!!!'
@@ -98,50 +136,52 @@ describe('Question API:', function () {
 				.expect('Content-Type', /json/)
 				.end(function (err, res) {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					updatedQuestion = res.body;
-					done();
-				});
-		});
+					updatedQuestion = res.body
+					done()
+				})
+		})
 
 		afterEach(function () {
-			updatedQuestion = {};
-		});
+			updatedQuestion = {}
+		})
 
 		it('should respond with the updated question', function () {
-			expect(updatedQuestion.title).to.equal('Updated Question');
-			expect(updatedQuestion.content).to.equal('This is the updated question!!!');
-		});
+			expect(updatedQuestion.title).to.equal('Updated Question')
+			expect(updatedQuestion.content).to.equal('This is the updated question!!!')
+		})
 
-	});
+	})
 
 	describe('DELETE /api/questions/:id', function () {
 
 		it('should respond with 204 on successful removal', function (done) {
 			request(app)
 				.delete('/api/questions/' + newQuestion._id)
+				.set('authorization', 'Bearer ' + token) // need auth
 				.expect(204)
 				.end((err, res) => {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					done();
-				});
-		});
+					done()
+				})
+		})
 
 		it('should respond with 404 when question does not exist', function (done) {
 			request(app)
 				.delete('/api/questions/' + newQuestion._id)
+				.set('authorization', 'Bearer ' + token) // need auth
 				.expect(404)
 				.end((err, res) => {
 					if (err) {
-						return done(err);
+						return done(err)
 					}
-					done();
-				});
-		});
+					done()
+				})
+		})
 
-	});
+	})
 
-});
+})
