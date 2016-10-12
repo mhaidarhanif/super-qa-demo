5a45c8f518e01143bad1a8be70444a3328d89cc6
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-12 19:48:08 +0700

    question+answer: name+createdAt

--- client/app/questionsShow/questionsShow.controller.js
+++ client/app/questionsShow/questionsShow.controller.js
@@ -2,11 +2,13 @@
 
 angular.module('superQa3App')
 	.controller('QuestionsShowCtrl', function ($scope, $http, $stateParams) {
+
 		let loadQuestions = function () {
 			$http.get('/api/questions/' + $stateParams.id).success(function (question) {
 				$scope.question = question
 			})
 		}
+
 		loadQuestions()
 
 		$scope.newAnswer = {}
@@ -16,4 +18,33 @@ angular.module('superQa3App')
 				$scope.newAnswer = {}
 			})
 		}
+
+		$scope.deleteQuestion = function () {
+			$http.delete('/api/questions/' + $stateParams.id).success(function () {
+				$location.path('/')
+			})
+		}
+
+		$scope.deleteAnswer = function (answer) {
+			$http.delete('/api/questions/' + $stateParams.id + '/answers/' + answer._id).success(function () {
+				loadQuestions()
+			})
+		}
+
+		$scope.updateQuestion = function () {
+			$http.put('/api/questions/' + $stateParams.id, $scope.question).success(function () {
+				loadQuestions()
+			})
+		}
+
+		$scope.updateAnswer = function (answer) {
+			$http.put('/api/questions/' + $stateParams.id + '/answers/' + answer._id, answer).success(function () {
+				loadQuestions()
+			})
+		}
+
+		$scope.isOwner = function (obj) {
+			return Auth.isLoggedIn() && obj && obj.user && obj.user._id === Auth.getCurrentUser()._id
+		}
+
 	})
