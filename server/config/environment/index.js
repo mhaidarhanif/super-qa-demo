20e8ad9cf951b8702c516752510466d91a93a6b8
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-13 16:44:09 +0700

    config: session

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -29,7 +29,7 @@ var all = {
 
   // Secret for session, you will want to change this and make it an environment variable
   secrets: {
-    session: 'super-qa3-secret'
+    session: process.env.SESSION_SECRET || 'superqa-secret'
   },
 
   // MongoDB connection options
