417e52dd9a8420c93140bef3d6dcef3a21c26c40
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-19 20:28:24 +0700

    .travis.yml: script

--- karma.conf.js
+++ karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'client/bower_components/ng-tags-input/ng-tags-input.js',
       'client/bower_components/angular-messages/angular-messages.js',
       'client/bower_components/moment/moment.js',
+      'client/bower_components/semantic/dist/semantic.min.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
